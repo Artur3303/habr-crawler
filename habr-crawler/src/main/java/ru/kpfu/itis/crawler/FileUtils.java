27b056b5099817841b2a1c2a64ddaae62761f06f
Artur Karpov <artik100313@gmail.com>
2019-02-19 09:34:45 +0300

    *****

--- habr-crawler/src/main/java/ru/kpfu/itis/crawler/FileUtils.java
+++ habr-crawler/src/main/java/ru/kpfu/itis/crawler/FileUtils.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 
 public class FileUtils {
 
-    private static final String LINK_STORAGE_PATH = "data/crawl/index.txt";
-    private static final String TEXT_STORAGE_PATH = "data/crawl/texts/";
+    private static final String LINK_STORAGE_PATH = "../data/crawl/index.txt";
+    private static final String TEXT_STORAGE_PATH = "../data/crawl/texts/";
 
     private static final Logger LOGGER = Logger.getLogger(FileUtils.class.getName());
 
